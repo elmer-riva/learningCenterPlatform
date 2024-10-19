48a59ef68a1cc3da4bbe5bea4362d5e75ef71ba1
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-19 16:29:26 -0500

    feat(courses): add learning path item entity.

---
+++ src/main/java/com/acme/center/platform/learning/domain/model/entities/LearningPathItem.java
@@ -0,0 +1,25 @@
+package com.acme.center.platform.learning.domain.model.entities;
+
+import com.acme.center.platform.shared.domain.model.entities.AuditableModel;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class LearningPathItem extends AuditableModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  //Identity field patron
+
+  //TODO: Set relationship to Course
+
+  @NotNull
+  private Long tutorialId;
+
+  @ManyToOne
+  @JoinColumn(name = "next_item_id")
+  private LearningPathItem nextItem;
+
+}
