51dd918b23c413c1ad8bc41dddaba4aa2b13a109
ValeryAyasta <102206366+ValeryAyasta@users.noreply.github.com>
2024-11-06 16:29:43 -0500

    feat(iam): add role seeding event handler.

---
+++ src/main/java/com/acme/center/platform/iam/application/internal/eventhandlers/ApplicationReadyEventHandler.java
@@ -0,0 +1,43 @@
+package com.acme.center.platform.iam.application.internal.eventhandlers;
+
+import com.acme.center.platform.iam.domain.model.commands.SeedRolesCommand;
+import com.acme.center.platform.iam.domain.services.RoleCommandService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+
+/**
+ * Event handler for application ready event.
+ * <p>
+ * This event handler is responsible for verifying if roles seeding is needed when the application is ready.
+ * If roles seeding is needed, it will trigger the {@link SeedRolesCommand} to seed the roles.
+ * This event handler is triggered when the application is ready.
+ * It is used to seed the roles when the application is started.
+ * </p>
+ */
+@Service
+public class ApplicationReadyEventHandler {
+  private final RoleCommandService roleCommandService;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationReadyEventHandler.class);
+
+  public ApplicationReadyEventHandler(RoleCommandService roleCommandService) {
+    this.roleCommandService = roleCommandService;
+  }
+
+  @EventListener
+  public void on(ApplicationReadyEvent event) {
+    var applicationName = event.getApplicationContext().getId();
+    LOGGER.info("Starting to verify if roles seeding is needed for {} at {}", applicationName, currentTimestamp());
+    var seedRolesCommand = new SeedRolesCommand();
+    roleCommandService.handle(seedRolesCommand);
+    LOGGER.info("Finished verifying if roles seeding is needed for {} at {}", applicationName, currentTimestamp());
+  }
+
+  private Timestamp currentTimestamp() {
+    return new Timestamp(System.currentTimeMillis());
+  }
+}
